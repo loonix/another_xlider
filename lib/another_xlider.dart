9b10c9a8e0994c5b3062ab6bf64f3dd6b8a51b49
Felix Gabler <felix.gabler98@gmail.com>
2021-11-30 07:23:05 -0800

    Fix default parameter for small density
    (cherry picked from commit dcecd0231fb716d9f6856d2dacd084c55fbb53eb)

--- lib/another_xlider.dart
+++ lib/another_xlider.dart
@@ -2616,7 +2616,7 @@ class FlutterSliderHatchMark {
   FlutterSliderHatchMark(
       {this.disabled = false,
       this.density = 1,
-      this.smallDensity = 5,
+      this.smallDensity = 4,
       this.linesDistanceFromTrackBar,
       this.labelsDistanceFromTrackBar,
       this.labels,
